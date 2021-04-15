4c902891aac929ba3fa7568d2175e57f672eb899
F04C <81340347+F04C@users.noreply.github.com>
2021-04-15 11:30:04 +0800

    Exp8_2 Codes
    Added more radio buttons/ Changed Form Color, Text Color, and FontStyle

---
+++ Exp8_2.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Exp8_2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Exp8_2))
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton7 = New System.Windows.Forms.RadioButton()
+        Me.SuspendLayout()
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Font = New System.Drawing.Font("Verdana", 14.25!)
+        Me.RadioButton1.ForeColor = System.Drawing.Color.Red
+        Me.RadioButton1.Location = New System.Drawing.Point(12, 12)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(284, 27)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Change Label Color to Red"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Font = New System.Drawing.Font("Verdana", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton2.ForeColor = System.Drawing.Color.Orange
+        Me.RadioButton2.Location = New System.Drawing.Point(12, 45)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(317, 27)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Change Label Color to Orange"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Font = New System.Drawing.Font("Verdana", 14.25!)
+        Me.RadioButton3.ForeColor = System.Drawing.Color.Yellow
+        Me.RadioButton3.Location = New System.Drawing.Point(12, 78)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(311, 30)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Change Label Color to Yellow"
+        Me.RadioButton3.UseCompatibleTextRendering = True
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Font = New System.Drawing.Font("Verdana", 14.25!)
+        Me.RadioButton4.ForeColor = System.Drawing.Color.Green
+        Me.RadioButton4.Location = New System.Drawing.Point(12, 111)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(305, 27)
+        Me.RadioButton4.TabIndex = 4
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "Change Label Color to Green"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Verdana", 16.0!)
+        Me.Label1.Location = New System.Drawing.Point(120, 272)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(202, 26)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "github.com/F04C"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Font = New System.Drawing.Font("Verdana", 14.25!)
+        Me.RadioButton5.ForeColor = System.Drawing.Color.Blue
+        Me.RadioButton5.Location = New System.Drawing.Point(12, 144)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(290, 27)
+        Me.RadioButton5.TabIndex = 6
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.Text = "Change Label Color to Blue"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'RadioButton6
+        '
+        Me.RadioButton6.AutoSize = True
+        Me.RadioButton6.Font = New System.Drawing.Font("Verdana", 14.25!)
+        Me.RadioButton6.ForeColor = System.Drawing.Color.Indigo
+        Me.RadioButton6.Location = New System.Drawing.Point(12, 177)
+        Me.RadioButton6.Name = "RadioButton6"
+        Me.RadioButton6.Size = New System.Drawing.Size(310, 27)
+        Me.RadioButton6.TabIndex = 7
+        Me.RadioButton6.TabStop = True
+        Me.RadioButton6.Text = "Change Label Color to Indigo"
+        Me.RadioButton6.UseVisualStyleBackColor = True
+        '
+        'RadioButton7
+        '
+        Me.RadioButton7.AutoSize = True
+        Me.RadioButton7.Font = New System.Drawing.Font("Verdana", 14.25!)
+        Me.RadioButton7.ForeColor = System.Drawing.Color.Violet
+        Me.RadioButton7.Location = New System.Drawing.Point(12, 210)
+        Me.RadioButton7.Name = "RadioButton7"
+        Me.RadioButton7.Size = New System.Drawing.Size(303, 27)
+        Me.RadioButton7.TabIndex = 8
+        Me.RadioButton7.TabStop = True
+        Me.RadioButton7.Text = "Change Label Color to Violet"
+        Me.RadioButton7.UseVisualStyleBackColor = True
+        '
+        'Exp8_2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(464, 441)
+        Me.Controls.Add(Me.RadioButton7)
+        Me.Controls.Add(Me.RadioButton6)
+        Me.Controls.Add(Me.RadioButton5)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.RadioButton4)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Exp8_2"
+        Me.Text = "Exp8_2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents RadioButton6 As RadioButton
+    Friend WithEvents RadioButton7 As RadioButton
+End Class
---
+++ Exp8_2.vb
@@ -0,0 +1,89 @@
+﻿Module Module1
+    Private m_FormBackgroundColor As Color
+    Public Property FormBackgroundColor As Color
+        Get
+            Return m_FormBackgroundColor
+        End Get
+        Set(value As Color)
+            m_FormBackgroundColor = value
+            For Each Frm As Form In Application.OpenForms
+                Frm.BackColor = m_FormBackgroundColor
+            Next
+        End Set
+    End Property
+End Module
+Public Class Exp8_2
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+
+    End Sub
+
+    Private Sub RadioButton4_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton4.CheckedChanged
+
+    End Sub
+
+    Private Sub RadioButton5_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton5.CheckedChanged
+
+    End Sub
+
+    Private Sub RadioButton6_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton6.CheckedChanged
+
+    End Sub
+
+    Private Sub RadioButton7_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton7.CheckedChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged, RadioButton2.CheckedChanged, RadioButton3.CheckedChanged, RadioButton4.CheckedChanged, RadioButton5.CheckedChanged, RadioButton6.CheckedChanged, RadioButton7.CheckedChanged
+        If (RadioButton1.Checked) Then
+            Label1.ForeColor = Color.Red
+            Label1.Font = New Font("Papyrus", 16, FontStyle.Bold)
+        ElseIf (RadioButton2.Checked) Then
+            Label1.ForeColor = Color.Orange
+            Label1.Font = New Font("Poor Richard", 16, FontStyle.Bold)
+        ElseIf (RadioButton3.Checked) Then
+            Label1.ForeColor = Color.Yellow
+            Label1.Font = New Font("Vivaldi", 16, FontStyle.Bold)
+        ElseIf (RadioButton4.Checked) Then
+            Label1.ForeColor = Color.Green
+            Label1.Font = New Font("Cooper", 16, FontStyle.Bold)
+        ElseIf (RadioButton5.Checked) Then
+            Label1.ForeColor = Color.Blue
+            Label1.Font = New Font("Viner Hand ITC", 16, FontStyle.Bold)
+        ElseIf (RadioButton6.Checked) Then
+            Label1.ForeColor = Color.Indigo
+            Label1.Font = New Font("Castellar", 16, FontStyle.Bold)
+        ElseIf (RadioButton7.Checked) Then
+            Label1.ForeColor = Color.Violet
+            Label1.Font = New Font("Dubai", 16, FontStyle.Bold)
+        End If
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Me.BackColor = FormBackgroundColor
+    End Sub
+    Private Sub bgcblue_Click(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged, RadioButton2.CheckedChanged, RadioButton3.CheckedChanged, RadioButton4.CheckedChanged, RadioButton5.CheckedChanged, RadioButton6.CheckedChanged, RadioButton7.CheckedChanged
+        If (RadioButton1.Checked) Then
+            FormBackgroundColor = Color.Green
+        ElseIf (RadioButton2.Checked) Then
+            FormBackgroundColor = Color.Blue
+        ElseIf (RadioButton3.Checked) Then
+            FormBackgroundColor = Color.Violet
+        ElseIf (RadioButton4.Checked) Then
+            FormBackgroundColor = Color.Red
+        ElseIf (RadioButton5.Checked) Then
+            FormBackgroundColor = Color.Orange
+        ElseIf (RadioButton6.Checked) Then
+            FormBackgroundColor = Color.Gold
+        ElseIf (RadioButton7.Checked) Then
+            FormBackgroundColor = Color.Yellow
+        End If
+    End Sub
+End Class
+
